1fc662ffe4168704f35beda33af21d228076f1ec
Alejandro Hernández <info@alejandrohdezma.com>
2019-01-25 13:45:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+imprentacentenera.com
---
+++ js/main.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function () {
+    window.cookieconsent.initialise({
+        "palette": {
+            "popup": {
+                "background": "#000"
+            },
+            "button": {
+                "background": "#f1d600"
+            }
+        },
+        "position": "bottom-right",
+        "content": {
+            "message": "Utilizamos cookies para garantizar el correcto funcionamiento de la página. Si continúa navegando, acepta su uso.",
+            "dismiss": "¡Acepto!",
+            "link": "Leer más"
+        }
+    })
+});
